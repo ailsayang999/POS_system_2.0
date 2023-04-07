cf3d3c17de290ec0e4967085256bb01ac56941e8
AilsaYang <ailsayang831031@gmail.com>
2023-04-07 19:03:59 +0800

    feat: add "add to cart" function

--- app/js/script.js
+++ app/js/script.js
@@ -36,4 +36,48 @@ function displayProducts(products) {
     )
     .join("");
 }
+
+//function: add to cart 
+//function: 加入購物車
+function addToCart(event) {
+  // 找到觸發event的node元素，並得到其產品id
+  const id = event.target.id;
+  // 在productData的資料裡，找到點擊的產品(addedProduct)的資訊： name, price
+  const addedProduct = productData.find((item) => item.id === id);
+  // 如果按馬卡龍的話 addedProduct 會等於：{"id": "product-1","imgUrl": "https://images.unsplash.com/photo-1558024920-b41e1887dc32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",name": "馬卡龍","price": 90}
+  const addedProductName = addedProduct.name;
+  const addedProductPrice = addedProduct.price;
+
+  // 加入購物車變數cartItems 分：有按過、沒按過
+  const targetCartItem = cartItem.find((item) => item.id === id);
+  if (targetCartItem) {
+    //有按過的話(targetCartItem為true) quantity+=1
+    targetCartItem.quantity += 1;
+  } else {
+    // 沒按過 cartItem加入新資料  在新版本後，如果key和value的命名一樣的話，可以簡寫
+    cartItem.push({
+      id: id,
+      name: addedProductName,
+      price: addedProductPrice,
+      quantity: 1,
+    });
+  }
+
+  // 畫面顯示購物車清單 map會建立一個新的陣列，.join('')可以把陣列變成字串
+  cart.innerHTML = cartItem
+    .map(
+      (item) =>
+        `<span class="item">${item.name} X ${item.quantity} 小計：${
+          item.price * item.quantity
+        } 元</span>`
+    )
+    .join("");
+
+  // console.log(cartItem);
+
+  // 計算總金額
+  let price = 0;
+  cartItem.forEach((item) => (price += item.price * item.quantity));
+  calculateTotal(price);
+}
  
